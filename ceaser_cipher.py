9f42c04bd2680c8a0e6b571d21c9e71de3aa9e83
Rafael <89470801+glo-q@users.noreply.github.com>
2022-01-31 10:37:40 +0100

    Create ceaser_cipher.py

---
+++ ceaser_cipher.py
@@ -0,0 +1,9 @@
+def caesar_cipher(string, offset):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    decoded = ""
+    for char in string:
+        position = alphabet.index(char)
+        decode = alphabet[position - offset]
+        decoded += decode
+    return decoded
